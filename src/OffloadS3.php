9be10751b5d16a9d34f4f1b59f0a93486f9b0987
Magnus Flor Johansen <magnus@m-flor.dk>
2018-07-25 14:23:26 +0200

    Update Offload S3

--- src/OffloadS3.php
+++ src/OffloadS3.php
@@ -2,14 +2,58 @@
 
 namespace Bonnier\Willow\MuPlugins;
 
-
 class OffloadS3
 {
     public function __construct()
     {
+        add_action('option_active_plugins', [$this, 'activatePlugin']);
+
+        add_filter('as3cf_object_meta', [$this, 'setDownloadableFiles']);
+
         add_action('option_tantan_wordpress_s3', [$this, 'setOptions']);
     }
-    public function setOptions($currentOptions) {
+
+    public function activatePlugin($activePlugins)
+    {
+        $pluginToActivate = 'amazon-s3-and-cloudfront/wordpress-s3.php';
+
+        if (file_exists(WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $pluginToActivate)) {
+            $mustUsePlugins = [
+                'amazon-web-services/amazon-web-services.php',
+                $pluginToActivate
+            ];
+
+            // Make sure the $activePlugins only contains the must use plugin once
+            return array_unique(array_merge($activePlugins, $mustUsePlugins));
+        }
+
+        return $activePlugins;
+    }
+
+    public function setDownloadableFiles($args)
+    {
+        if (isset($args['Key']) && isset($args['ContentType'])) {
+            $contentTypes = [
+                'application/octet-stream'
+            ];
+            $fileExtensions = [
+                'lrtemplate',
+                'exe'
+            ];
+            if (preg_match('/([^.]+$)/', $args['Key'], $extensionMatches) &&
+                in_array($args['ContentType'], $contentTypes)
+            ) {
+                $extension = $extensionMatches[0];
+                if (in_array($extension, $fileExtensions)) {
+                    $args['ContentDisposition'] = 'attachment';
+                }
+            }
+        }
+        return $args;
+    }
+
+    public function setOptions($currentOptions)
+    {
         $overrideOptions = [
             'bucket' => env('AWS_S3_BUCKET', 'wp-uploads.interactives.dk'),
             'object-prefix' => env('AWS_S3_UPLOADS_PATH'),
