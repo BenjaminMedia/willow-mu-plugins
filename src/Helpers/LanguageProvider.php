e61bb27fa6148263527a50bd907f6d0d6981914c
Jonas Kjaergaard <jonask@jonask.dk>
2018-11-21 13:50:26 +0100

    IVD-436: Fixed incorrect protocol in wp_cli

--- src/Helpers/LanguageProvider.php
+++ src/Helpers/LanguageProvider.php
@@ -10,11 +10,15 @@ class LanguageProvider
      */
     public static function registerSubdomain()
     {
-        if (!is_admin()) {
+        if (!is_admin() && !defined('WP_CLI')) {
             add_filter('option_polylang', function ($options) {
                 foreach ($options['domains'] as $locale => $domain) {
                     if (str_contains(parse_url($domain, PHP_URL_HOST), self::getServerBaseHost())) {
-                        $subDomain = sprintf('%s://%s', parse_url($domain, PHP_URL_SCHEME), $_SERVER['HTTP_HOST']);
+                        $subDomain = sprintf(
+                            '%s://%s',
+                            parse_url($domain, PHP_URL_SCHEME),
+                            parse_url($_SERVER['HTTP_HOST'], PHP_URL_HOST)
+                        );
                         $options['domains'][$locale] = $subDomain;
                     }
                 }
